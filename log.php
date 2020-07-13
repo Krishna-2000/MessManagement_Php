738b85a2ec064ca84e318b9f7c76c17c60ef1784
Shabin Kv <shabinkv3@gmail.com>
2020-07-13 21:08:49 +0530

    ionic seems to work

---
+++ log.php
@@ -0,0 +1,31 @@
+<html>
+  <head>
+    <script
+      type="module"
+      src="https://cdn.jsdelivr.net/npm/@ionic/core@next/dist/ionic/ionic.esm.js"
+    ></script>
+    <script
+      nomodule
+      src="https://cdn.jsdelivr.net/npm/@ionic/core@next/dist/ionic/ionic.js"
+    ></script>
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/@ionic/core@next/css/ionic.bundle.css"
+    />
+  </head>
+  <body>
+    <ion-content>
+      <ion-grid>
+        <ion-row>
+          <ion-col offset="4">
+            <ion-card>
+              <ion-card-content>
+                <ion-label><?php echo "hi"; ?></ion-label>
+              </ion-card-content>
+            </ion-card>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
+    </ion-content>
+  </body>
+</html>
